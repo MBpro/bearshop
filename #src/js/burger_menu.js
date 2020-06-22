db79306e997ff5af487d0d366ae16ca78ee95ce2
MBpro <65220848+MBpro@users.noreply.github.com>
2020-06-22 11:35:48 +0200

    added tablet navigation

--- #src/js/burger_menu.js
+++ #src/js/burger_menu.js
@@ -1,12 +1,17 @@
-let burger = document.querySelector('#toggle').onclick = function () {
-    document.querySelector('.main-nav__span').classList.toggle('active');
-    document.querySelector('.main-nav__burger').classList.toggle('active');
-    document.querySelector('.main-nav__wrapper').classList.toggle('active');
+let onJsBurger = document.querySelector('.main-nav__burger').classList.add('js-main-nav__burger');//class for styling burger button ONjs
 
-    let a = document.querySelectorAll('.main-nav__list--closed');
-    for (let i = 0; i < a.length; i++) {
-        a[i].classList.toggle('opened');
-    }
-
-    //document.querySelector('body').classList.toggle('lock');
+let onJsMenuClosed = document.querySelectorAll('.main-nav__list--offjs');//menu closed ONjs
+for (let i = 0; i < onJsMenuClosed.length; i++) {
+    onJsMenuClosed[i].classList.add('js-main-nav__list--closed');
 }
+
+let burgerButton = document.querySelector('#toggle').onclick = function () {
+    document.querySelector('.main-nav__span').classList.toggle('js-active');//hiding horizontal line ONjs
+    document.querySelector('.main-nav__burger').classList.toggle('js-active');//closing icon-X ONjs
+
+    let onJsMenuOpened = document.querySelectorAll('.main-nav__list--offjs');//menu opened ONjs
+    for (let i = 0; i < onJsMenuOpened.length; i++) {
+        onJsMenuOpened[i].classList.toggle('js-main-nav__list--opened');
+    }
+}
+
