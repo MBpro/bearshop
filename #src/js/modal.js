a078f098b382857f91e2525b23b2acb7557ae33c
MBpro <65220848+MBpro@users.noreply.github.com>
2020-08-12 19:25:14 +0200

    finish

--- #src/js/modal.js
+++ #src/js/modal.js
@@ -1,16 +1,18 @@
 
-let link = document.querySelector("#modal-on");
+let link = document.querySelectorAll(".modal-on");
 let popup = document.querySelector('.modal-cart');
 let backgroundModal = document.querySelector('.background-modal');
 let buttonClose = document.querySelector('#button-close');
 let bodyLock = document.querySelector('body');
 
-link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add('js-modal-cart-show');
-    backgroundModal.classList.add('js-background-modal-show');
-    bodyLock.classList.add('body-lock');
-})
+for (i = 0; i < link.length; i++) {
+    link[i].addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add('js-modal-cart-show');
+        backgroundModal.classList.add('js-background-modal-show');
+        bodyLock.classList.add('body-lock');
+    })
+}
 
 buttonClose.addEventListener('click', function (evt) {
     evt.preventDefault();
